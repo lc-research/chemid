8b550e1ecd82b2c4a231eeb74fdb942c80860660
Lochana Menikarachchi <lochanac@gmail.com>
2015-08-15 20:52:59 +0530

    initial commit

---
+++ src/main/java/org/chemid/structure/restapi/ChemicalStructureServiceRESTAPI.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2015, ChemID. (http://www.chemid.org)
+ *
+ * ChemID licenses this file to you under the Apache License V 2.0.
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package org.chemid.structure.restapi;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * This class includes RESTful API methods for chemical structure service.
+ */
+@Path("/rest")
+public class ChemicalStructureServiceRESTAPI {
+    /**
+     * This method returns the version number of the chemical structure service.
+     *
+     * @return API version
+     */
+    @GET
+    @Path("version")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String version() {
+        return "Chemical Structure Service V 1.0";
+    }
+}
