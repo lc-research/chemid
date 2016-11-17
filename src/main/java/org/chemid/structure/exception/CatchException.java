e2ebbe2aed7a20602a5f06afc7fd18b002579c17
Kaushi <rkaushi413@gmail.com>
2016-11-17 15:52:56 +0530

    added exception

---
+++ src/main/java/org/chemid/structure/exception/CatchException.java
@@ -0,0 +1,10 @@
+package org.chemid.structure.exception;
+
+/**
+ * Created by Kaushi on 11/17/2016.
+ */
+public class CatchException extends Exception{
+    public CatchException(String message) {
+        super(message);
+    }
+}
