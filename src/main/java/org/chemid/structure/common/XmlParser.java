ca3003004dfd5e99b08fe21a140d8e9e128c99d0
Kaushi <rkaushi413@gmail.com>
2016-11-25 13:32:18 +0530

    added exception and handle null return

--- src/main/java/org/chemid/structure/common/XmlParser.java
+++ src/main/java/org/chemid/structure/common/XmlParser.java
@@ -11,17 +11,21 @@
  */
 package org.chemid.structure.common;
 
+import org.chemid.structure.exception.CatchException;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.net.URL;
@@ -29,30 +33,45 @@ import java.net.URL;
 public class XmlParser {
 
 
-    public static Document StringToXML(String xmlRecords) throws Exception {
+    public static Document StringToXML(String xmlRecords) throws CatchException {
 
-        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        InputSource is = new InputSource();
-        is.setCharacterStream(new StringReader(xmlRecords));
+        DocumentBuilder db = null;
+        Document doc = null;
+        try {
+            db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            InputSource is = new InputSource();
+            is.setCharacterStream(new StringReader(xmlRecords));
 
-        Document doc = db.parse(is);
+            doc = db.parse(is);
+        } catch (Exception e) {
+            throw new CatchException("Error occurred in xml Parser StringToXML : " + e.getMessage());
+        }
+
         return doc;
     }
 
-    public static Document getXMLPayload(String fileName, String location) throws Exception {
-
+    public static Document getXMLPayload(String fileName, String location) throws CatchException {
+        Document doc = null;
 //        URL url = Thread.currentThread().getContextClassLoader().getResource(location + fileName);
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
-        URL resource = classLoader.getResource(location + fileName);
+            URL resource = classLoader.getResource(location + fileName);
 
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(new File(resource.getPath()));
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = null;
+
+            dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(new File(resource.getPath()));
+
+        } catch (Exception e) {
+            throw new CatchException("Error occurred in xml Parser getXMLPayload : " + e.getMessage());
+        }
         return doc;
     }
 
-    public static String getStringFromDocument(Document doc) {
+    public static String getStringFromDocument(Document doc) throws CatchException {
+        String output = null;
         try {
             DOMSource domSource = new DOMSource(doc);
             StringWriter writer = new StringWriter();
@@ -60,12 +79,10 @@ public class XmlParser {
             TransformerFactory tf = TransformerFactory.newInstance();
             Transformer transformer = tf.newTransformer();
             transformer.transform(domSource, result);
-            return writer.toString();
+            output = writer.toString();
+        } catch (Exception ex) {
+            throw new CatchException("Error occurred in xml Parser getStringFromDocument : " + ex.getMessage());
         }
-        catch(TransformerException ex)
-        {
-            ex.printStackTrace();
-            return null;
-        }
+        return output;
     }
 }
