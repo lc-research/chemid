b84ec5d468d2b1b8a70ae96011871d14c7c4c393
Lochana Menikarachchi <lochanac@gmail.com>
2018-05-20 16:40:51 +0530

    Changes to Copyright

--- cheminformatics/src/main/java/org/chemid/cheminformatics/FileIO.java
+++ cheminformatics/src/main/java/org/chemid/cheminformatics/FileIO.java
@@ -1,3 +1,15 @@
+/*
+ *  Copyright (c) 2018, LC-Research. (http://www.lc-research.com)
+ *
+ *  LC-Research licenses this file to you under the Apache License V 2.0.
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ *  Unless required by applicable law or agreed to in writing, software distributed under the
+ *  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *  CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations under the License.
+ */
+
 package org.chemid.cheminformatics;
 
 import org.openscience.cdk.DefaultChemObjectBuilder;
@@ -13,7 +25,7 @@ public class FileIO {
 
     /**
      *
-      * @param FilePathToSDF File Path to SD File
+     * @param FilePathToSDF File Path to SD File
      * @return Method Returns an Iterator of IAtomContainers
      * @throws FileNotFoundException
      */
--- structure/src/main/java/org/chemid/structure/exception/ChemIDStructureException.java
+++ structure/src/main/java/org/chemid/structure/exception/ChemIDStructureException.java
@@ -1,7 +1,7 @@
 /*
  *  Copyright (c) 2018, LC-Research. (http://www.lc-research.com)
  *
- *  ChemID licenses this file to you under the Apache License V 2.0.
+ *  LC-Research licenses this file to you under the Apache License V 2.0.
  *  You may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
  *  Unless required by applicable law or agreed to in writing, software distributed under the
