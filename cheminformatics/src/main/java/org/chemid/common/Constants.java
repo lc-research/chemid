d6f1ca6e5e2351e1fab3353551d672f0f796d796
LahiRumesh <lahrumesh28@gmail.com>
2018-07-17 10:38:13 +0530

    cheminformatics Constants

---
+++ cheminformatics/src/main/java/org/chemid/common/Constants.java
@@ -0,0 +1,16 @@
+package org.chemid.common;
+
+public class Constants {
+    public static final String LOCATION_SEPARATOR = "\\";
+    public static final String LOCATION_SEPARATOR_LEFT = "/";
+    public static final String SDF_FILE_NAME = "yyyyMMddhhmm'.sdf'";
+    public static final String CHEMSPIDER_CSID = "CSID";
+    public static final String HMDB_ID = "ID";
+    public static final String FILE_NOT_FOUND = "Resource file not found!";
+    public static final String PUBCHEM_ISOTOPIC_ATOM_COUNT = "PUBCHEM_ISOTOPIC_ATOM_COUNT";
+    public static final String PUBCHEM_COMPOUND_CID = "PUBCHEM_COMPOUND_CID";
+    public static final String GENERATED_SMILE = "GENERATED_SMILE";
+    public static final String EMPTY_MSG = "0 Compounds for given filters";
+
+
+}
