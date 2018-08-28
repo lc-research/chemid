903c4dd2e4e346ae2eaeb2e29b0988597e22c98e
LahiRumesh <lahrumesh28@gmail.com>
2018-08-28 12:21:16 +0530

    id module Constants class

--- id/src/main/java/org/chemid/id/common/Constants.java
+++ id/src/main/java/org/chemid/id/common/Constants.java
@@ -1,4 +1,8 @@
 package org.chemid.id.common;
 
 public class Constants {
+    public static final int PORT_NO = 8080;
+    public static final int INIT_ORDER = 0;
+    public static final String FILE_PATH_EMPTY = "Please specify file path";
+
 }
