0e436f6f18248eb3de82ecd3acaa24c8ee95a149
LahiRumesh <lahrumesh28@gmail.com>
2018-10-10 12:10:40 +0530

    Changes to preFilter Constants

--- preFilter/src/main/java/org/chemid/prefilter/common/Constants.java
+++ preFilter/src/main/java/org/chemid/prefilter/common/Constants.java
@@ -14,17 +14,7 @@ package org.chemid.prefilter.common;
 public class Constants {
     public static final int PORT_NO = 8080;
     public static final int INIT_ORDER = 0;
-    public static final String LOCATION_SEPARATOR = "\\";
-    public static final String LOCATION_SEPARATOR_LEFT = "/";
-    public static final String SDF_FILE_NAME = "yyyyMMddhhmm'.sdf'";
-    public static final String CHEMSPIDER_CSID = "CSID";
-    public static final String HMDB_ID = "ID";
-    public static final String FILE_NOT_FOUND = "Resource file not found!";
-    public static final String PUBCHEM_ISOTOPIC_ATOM_COUNT = "PUBCHEM_ISOTOPIC_ATOM_COUNT";
-    public static final String PUBCHEM_COMPOUND_CID = "PUBCHEM_COMPOUND_CID";
     public static final String FILE_PATH_EMPTY = "Please specify file path";
-    public static final String GENERATED_SMILE = "GENERATED_SMILE";
-    public static final String EMPTY_MSG = "0 Compounds for given filters";
     public static final String ZERO_COMPOUNDS_ERROR_LOG_PREFILTER = "something going wrong.No compounds according to the filters";
 
 
